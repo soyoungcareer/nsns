a63ee20496aad17af7e77e9b73d725c04e5dee83
82108 <82108@DESKTOP-AJGI7JR>
2021-10-17 00:17:20 +0900

    예경님 꺼

---
+++ src/main/java/com/kh/spring/notice/controller/NoticeController.java
@@ -0,0 +1,42 @@
+package com.kh.spring.notice.controller;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.kh.spring.common.Pagination;
+import com.kh.spring.notice.model.service.NoticeService;
+import com.kh.spring.notice.model.vo.Notice;
+import com.kh.spring.notice.model.vo.PageInfo;
+
+@Controller
+public class NoticeController {
+	
+	@Autowired
+	private NoticeService noticeService;
+	/*
+	@RequestMapping("list.ntc")
+	public String noticeTest() {
+		return "admin/noticeListView";
+	}*/
+	
+	@RequestMapping("list.ntc")
+	public String selectList(@RequestParam(value="currentPage", required = false, defaultValue="1") int currentPage , Model model) {
+		
+		int listCount = noticeService.selectNListCount();
+		System.out.println(listCount);
+		PageInfo pi = Pagination.getPageInfo(listCount, currentPage, 10, 5);
+		
+		ArrayList<Notice> list = noticeService.selectNList(pi);
+		System.out.println("list : " + list);
+		model.addAttribute("list", list);
+		model.addAttribute("pi", pi);
+		
+		return "admin/noticeListView";
+	}
+
+}
