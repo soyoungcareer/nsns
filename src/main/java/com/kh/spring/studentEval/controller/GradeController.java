343acd6dca234198dde30231332bd3bdfb19db1f
KyoungJun1 <81546338+KyoungJun1@users.noreply.github.com>
2021-10-21 22:52:00 +0900

    Update GradeController.java

--- src/main/java/com/kh/spring/studentEval/controller/GradeController.java
+++ src/main/java/com/kh/spring/studentEval/controller/GradeController.java
@@ -7,47 +7,42 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.kh.spring.common.Pagination;
-import com.kh.spring.notice.model.vo.PageInfo;
+import com.kh.spring.notice.vo.PageInfo;
 import com.kh.spring.studentEval.service.GradeService;
 import com.kh.spring.studentEval.vo.Grade;
 
-
-//@Controller
-
+@Controller
 public class GradeController {
 /*
 	@Autowired
 	private GradeService gradeService;
 	
-	/*
 	// 교수 > 성적 관리 > 성적 등록/조회/수정
 	@RequestMapping("profGradeMain.pr")
 	public String profGradeMain() {
 		return "professor/profGradeMain";
 	}
-	*/
-	
+	/*
 	// 성적 조회
-	@RequestMapping("profGradeMain.pr")
-	public String selectList(@RequestParam(value="currentPage", required=false, defaultValue="1") 
-								int currentPage, Model model) {
+	@RequestMapping()
+	public String selectList(@RequestParam(value="currentPage", required=false, defaultValue="1") int currentPage, Model model) {
 		int listCount = gradeService.selectListCount();
 		
-		String subCode = "2101001";
-		int gradeYear = 2021;
-		int gradeSemester = 1;
-		
 		PageInfo pi = Pagination.getPageInfo(listCount, currentPage, 10, 5);
-		ArrayList<Grade> list = gradeService.selectList(pi, subCode, gradeYear, gradeSemester);
 		
-		model.addAttribute("pi", pi);
+		ArrayList<Grade> list = gradeService.selectList(pi);
 		model.addAttribute("list", list);
+		model.addAttribute("pi", pi);
 		
 		return "professor/profGradeMain";
 	}
+	*/
 	/*
+	 *
+
 	@RequestMapping()
 	public ModelAndView selectGrade(int gno, ModelAndView mv) {
 		Grade g = gradeService.selectGrade(gno);
@@ -55,15 +50,14 @@ public class GradeController {
 		mv.addObject("g", g).setViewName("professor/profGradeMain");
 		return mv;
 	}
-	
-
-	// 성적 등록
+	*/
+	/*
+	// 성적 등록s
 	@RequestMapping()
 	public String insertGrade(Grade g, HttpServletRequest request, Model model,
 								@RequestParam) {
 		
 	}
-	*/
 	
 	// 교수 > 성적 관리 > 이의신청 확인
 	@RequestMapping("profGradeObj.pr")
@@ -75,9 +69,7 @@ public class GradeController {
 	@RequestMapping("profGradeObjDetail.pr")
 	public String profGradeObjDetail() {
 		return "professor/profGradeObjDetail";
-
-
-	}*/
-
-
+	}
+	
+	*/
 }
