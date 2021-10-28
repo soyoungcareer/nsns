5ba444bc11d81c0b9764b1790f6f5e64b396b417
KyoungJun1 <81546338+KyoungJun1@users.noreply.github.com>
2021-10-28 23:24:56 +0900

    2

---
+++ src/main/java/com/kh/spring/facility/model/vo/allam.java
@@ -0,0 +1,24 @@
+package com.kh.spring.facility.model.vo;
+
+import java.sql.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class allam {
+	
+	private int aNo;
+	private int stuId;
+	private String status;
+	private String name;
+	
+
+}
